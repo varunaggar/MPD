dcb4614f2ca019fce1877e717cfdd000bae6ee78
vaggar123 <165725291+vaggar123@users.noreply.github.com>
2026-05-24 20:15:26 +0100

    SQL-testing

--- WIP/sql/Add-SP.sql
+++ WIP/sql/Add-SP.sql
@@ -2,3 +2,5 @@ CREATE USER [ori19] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [ori19];
 ALTER ROLE db_datawriter ADD MEMBER [ori19];
 GRANT EXECUTE TO [ori19];
+
+Select * from DeltaTokens
